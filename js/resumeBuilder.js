3cbd1a0f89f552970b8129696a82915aa8fd75ed
joegallivan@talktalk.net <joegallivan@talktalk.net>
2014-11-15 21:29:51 +0000

    Use string.replace and append - Lesson 1.3

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,5 @@
-$("#main").append("Joe Gallivan");
+//$("#main").append("Joe Gallivan");
+var awesomeThoughts = "I am Joe ";
+awesomeThoughts +="and I am AWESOME!!!";
+funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+$("#main").append(funThoughts);
