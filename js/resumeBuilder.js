128923f501fd96e95cdae099cb3e7cec120cd3e3
joegallivan@talktalk.net <joegallivan@talktalk.net>
2014-11-15 21:58:44 +0000

    Add name and role via prepend - Lesson 1.5

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,10 @@
 //$("#main").append("Joe Gallivan");
-var awesomeThoughts = "I am Joe ";
-awesomeThoughts +="and I am AWESOME!!!";
-funThoughts = awesomeThoughts.replace("AWESOME","FUN");
-$("#main").append(funThoughts);
+//var awesomeThoughts = "I am Joe ";
+//awesomeThoughts +="and I am AWESOME!!!";
+//funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+//$("#main").append(funThoughts);
+var formattedName=HTMLheaderName.replace("%data%","Joe Gallivan");
+var formattedRole=HTMLheaderRole.replace("%data%","Learning Facilitator");
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
