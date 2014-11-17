25c1c488011789e1cc2e21e45f64cd2f4436920b
joegallivan@talktalk.net <joegallivan@talktalk.net>
2014-11-17 20:49:09 +0000

    Add 1st use of object - picture, welcome message etc.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,8 +3,28 @@
 //awesomeThoughts +="and I am AWESOME!!!";
 //funThoughts = awesomeThoughts.replace("AWESOME","FUN");
 //$("#main").append(funThoughts);
-var formattedName=HTMLheaderName.replace("%data%","Joe Gallivan");
-var formattedRole=HTMLheaderRole.replace("%data%","Learning Facilitator");
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+//var formattedName=HTMLheaderName.replace("%data%","Joe Gallivan");
+//var formattedRole=HTMLheaderRole.replace("%data%","Learning Facilitator");
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+
+// Now set up bio information
+
+var bio = {
+	formattedName:HTMLheaderName.replace("%data%","Joe Gallivan"),
+	formattedRole:HTMLheaderRole.replace("%data%","Learning Facilitator"),
+	formattedContact:HTMLemail.replace("%data%","joegallivan@talktalk.net"),
+	formattedBioPic:HTMLbioPic.replace("%data%","js/me.jpg"),
+	formattedWelcomeMsg:HTMLWelcomeMsg.replace("%data%","Welcome to my resume"),
+	formattedSkills:HTMLskills.replace("%data%",["Mentor"," Note Taker"," Sometime Programmer"].toString()),
+
+	fullBio:function() {
+		return this.formattedName +
+		this.formattedRole + this.formattedBioPic +
+		this.formattedContact  +
+		HTMLskillsStart + this.formattedSkills + this.formattedWelcomeMsg;
+		}
+};
+$("#header").prepend(bio.fullBio());
+
 
