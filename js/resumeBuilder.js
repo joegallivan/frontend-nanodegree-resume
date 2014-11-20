b6bc95e5ff23934f9a739634072b6790f780da6e
joegallivan@talktalk.net <joegallivan@talktalk.net>
2014-11-20 20:53:07 +0000

    Tidy up resume-builder after lesson 1.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,65 +1,136 @@
-//$("#main").append("Joe Gallivan");
-//var awesomeThoughts = "I am Joe ";
-//awesomeThoughts +="and I am AWESOME!!!";
-//funThoughts = awesomeThoughts.replace("AWESOME","FUN");
-//$("#main").append(funThoughts);
-//var formattedName=HTMLheaderName.replace("%data%","Joe Gallivan");
-//var formattedRole=HTMLheaderRole.replace("%data%","Learning Facilitator");
-//$("#header").prepend(formattedRole);
-//$("#header").prepend(formattedName);
 
-// Now set up bio information
 
+// Bio object
 var bio = {
-	formattedName:HTMLheaderName.replace("%data%","Joe Gallivan"),
-	formattedRole:HTMLheaderRole.replace("%data%","Learning Facilitator"),
-	formattedContact:HTMLemail.replace("%data%","joegallivan@talktalk.net"),
-	formattedBioPic:HTMLbioPic.replace("%data%","js/me.jpg"),
-	formattedWelcomeMsg:HTMLWelcomeMsg.replace("%data%","Welcome to my resume"),
-	formattedSkills:HTMLskills.replace("%data%",["Mentor"," Note Taker"," Sometime Programmer"].toString()),
-
-	fullBio:function() {
-		return this.formattedName +
-		this.formattedRole + this.formattedBioPic +
-		this.formattedContact  +
-		HTMLskillsStart + this.formattedSkills + this.formattedWelcomeMsg;
-		}
+	    "name" : "Joe Gallivan",
+	    "role" : "Learning Facilitator",
+	    "welcomeMessage" : "Welcome to my CV",
+	    "contacts" : {
+	    	"mobile" : "fill in later",
+	    	"email" : "joegallivan@talktalk.net",
+	    	"github" : "joegallivan",
+	    	"location" : "Southport, UK"
+	    },
+	    "skills" : [
+	        "Mentor",
+	        "Note Taker",
+	        "Sometime Programmer",
+	        "IT Development Manager",
+	        "IT Project Manager"]
 };
-$("#header").prepend(bio.fullBio());
-// work object
-var work = {};
-work.position = "Learning Facilitator";
-work.employer = "Edge Hill University";
-work.yearsWorked = 8;
-work.place = "Ormskirk, UK";
-//$("#main").append(HTMLworkStart);
-$("#main").append(HTMLworkTitle.replace("%data%",work["position"]))
-// education object
-
-//$("#main").append(HTMLschoolName.replace("%data%",education.lastSchool));
+// Work object
+var work = {
+	"jobs" : [
+	{
+		"employer" : "Edge Hill University",
+		"title" : "Learning Facilitator",
+		"location" : "Ormskirk, UK",
+		"dateStarted" : 2006,
+		"dateFinished" : 2015,
+		"description" : "Help students with disabilities"
+	},
+	{
+		"employer" : "Alliance & Leicester",
+		"title" : "Development Manager",
+		"location" : "Liverpool, UK",
+		"dateStarted" : 1980,
+		"dateFinished" : 2006,
+		"description" : "Manage IT projects, support and maintenance"
+	},
+	{
+		"employer" : "F W Woolworth",
+		"title" : "Systems Programmer",
+		"location" : "Rochdale, UK",
+		"dateStarted" : 1978,
+		"dateFinished" : 1980,
+		"description" : "Support the organisation's operating system"
+	},
+	{
+		"employer" : "UK Department of Health",
+		"title" : "Analyst Programmer",
+		"location" : "Fleetwood, UK",
+		"dateStarted" : 1974,
+		"dateFinished" : 1978,
+		"description" : "Design and develop programs to specification"
+	}
+	]
+};
+// Education object
 var education = {
 	"schools" : [
 	{
 		"name": "Liverpool John Moores University",
-		"city": "Liverpool, UK",
+		"location": "Liverpool, UK",
 		"degree": "MBA",
-		"major" : ["Business Studies"],
-		"graduation" : 1995
+		"majors" : ["Business Studies"],
+		"dateStarted" : 1992,
+		"dateGraduated" : 1995,
+		"url" : "http://www.ljmu.ac.uk"
 	},
 	{
 		"name": "Liverpool Polytechnic",
-		"city": "Liverpool, UK",
+		"location": "Liverpool, UK",
 		"degree": "BSC(Econ)",
-		"major": ["Economics"],
-		"graduation" : 1974
+		"majors": ["Economics"],
+		"dateStarted" : 1971,
+		"dateGraduated" : 1974,
+		"url" : "http://www.ljmu.ac.uk"
 	}
 	],
 	"onlineCourses" : [
 	{
 		"title": "JavaScript Syntax",
 		"school": "Udacity",
-		"date" : 2014,
+		"dateStarted" : 2014,
+		"dateCompleted" : 2014,
 		"url" : "http://www.udacity.com/course/ud804"
+	},
+	{
+		"title": "Intro to Java Programming",
+		"school": "Udacity",
+		"dateStarted" : 2013,
+		"dateCompleted" : 2013,
+		"url" : "http://www.udacity.com/course/cs046"
+	},
+	{
+		"title": "Intro to Computer Science",
+		"school": "Udacity",
+		"dateStarted" : 2012,
+		"dateGraduated" : 2013,
+		"url" : "http://www.udacity.com/course/cs101"
+	}
+	]
+};
+// Projects Object
+var projects = {
+	"projects" : [
+	{
+		"title" : "XP Migration",
+		"dateStarted" : 2004,
+		"dateCompleted" : 2006,
+		"description" : "Manage the migration of the organisation's client computers to Windows XP",
+		"url" : "To be added later"
+	},
+	{
+		"title" : "Integrated Financial and Administrative Systems",
+		"dateStarted" : 1997,
+		"dateCompleted" : 2000,
+		"description" : "Manage parts of the migration of the organisation's financial, accounting and administrative systems to Oracle Applications",
+		"url" : "To be added later"
+	},
+	{
+		"title" : "Centralised Support",
+		"dateStarted" : 2000,
+		"dateCompleted" : 2002,
+		"description" : "Manage a project to reorganise the maintenance and support of the organisation's major applications",
+		"url" : "To be added later"
+	},
+	{
+		"title" : "Corporate Charging",
+		"dateStarted" : 1992,
+		"dateCompleted" : 1994,
+		"description" : "Manage the testing andimplementation of a bespoke application to charge corporate bank customers",
+		"url" : "To be added later"
 	}
 	]
 };
